728316d6e7ff64b4129b8ceed3b8d6f79658b17a
BochraFeki <56472087+Bochra906@users.noreply.github.com>
2022-03-01 22:11:30 +0100

    Update and rename SpaceResource.java to SpaceResourceAllocator.java

--- OCP/src/com/directi/training/ocp/exercise_refactored/SpaceResourceAllocator.java
+++ OCP/src/com/directi/training/ocp/exercise_refactored/SpaceResourceAllocator.java
@@ -11,12 +11,13 @@ public class SpaceResource implements Resource
     @Override
     public void markBusy(int resourceId)
     {
-
+       System.out.println("Marking space resource with id " + resourceId + "busy\n");
     }
 
     @Override
     public void markFree(int resourceId)
     {
+        System.out.println("Marking space resource with id " + resourceId + "free\n");
 
     }
 }
