5c4e9546842f692f60c8bf05287a57e7e9b7a555
BochraFeki <56472087+Bochra906@users.noreply.github.com>
2022-03-01 22:12:43 +0100

    Update and rename TimeResource.java to TimeResourceAllocator.java

--- OCP/src/com/directi/training/ocp/exercise_refactored/TimeResourceAllocator.java
+++ OCP/src/com/directi/training/ocp/exercise_refactored/TimeResourceAllocator.java
@@ -11,12 +11,14 @@ public class TimeResource implements Resource
     @Override
     public void markBusy(int resourceId)
     {
+        System.out.println("Marking space resource with id " + resourceId + "busy\n");
 
     }
 
     @Override
     public void markFree(int resourceId)
     {
+         System.out.println("Marking space resource with id " + resourceId + "free\n");
 
     }
 }
